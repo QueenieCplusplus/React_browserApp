679c47e05e856f639db6222e34066b66e868f432
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2020-07-20 18:38:22 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Button, Text, View } from 'react-native';
+import * as WebBrowser from 'expo-web-browser';
+
+export default class App extends Component {
+  state = {
+    result: null,
+  };
+
+  render() {
+    return (
+      <View>
+        <Button title="Open WebBrowser" onPress={this._pressHandler} />
+        <Text>{this.state.result && JSON.stringify(this.state.result)}</Text>
+      </View>
+    );
+  }
+
+  _pressHandler = async () => {
+    let result = await WebBrowser.openBrowserAsync('https://www.google.com/');
+    this.setState({ result });
+  };
+}
